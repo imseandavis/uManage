6f1c48ad7cd601e71e3d4470c3fb0c0d675a78e2
Brent Pabst <me@brentpabst.com>
2016-02-24 15:03:12 -0500

    Initial server

--- uManage/Program.cs
+++ uManage/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using NLog;
+using System;
+using System.Configuration;
 
 namespace S203.uManage
 {
@@ -10,6 +9,42 @@ namespace S203.uManage
     {
         static void Main(string[] args)
         {
+            Logger.StartLogging();
+            var logger = LogManager.GetLogger("umanage-server");
+
+            try
+            {
+                logger.Info("Starting uManage Server");
+
+                var baseAddress = ConfigurationManager.AppSettings["baseUri"];
+
+                if (string.IsNullOrWhiteSpace(baseAddress))
+                    throw new ArgumentNullException(baseAddress, "Base address not specified, check app.config and ensure the baseUri appSetting is specified.");
+
+                logger.Info("Base address set: {0}", baseAddress);
+
+                logger.Info("Starting web server...");
+
+                using (WebApp.Start<Startup>(baseAddress))
+                {
+                    logger.Info("Web server is ready to serve!");
+
+                    if (Environment.UserInteractive)
+                        logger.Info("Press any key to exit... ");
+
+                    Console.ReadLine();
+
+                    logger.Warn("Stopping server...");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "Unhandled error, server has terminated!");
+            }
+            finally
+            {
+                logger.Info("uManage is stoped!");
+            }
         }
     }
 }
